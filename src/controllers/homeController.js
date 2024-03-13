1e156d32aeab07f40b37148252c7c62dd4dba56b
le john <lejohn2801@gmail.com>
2024-03-13 16:55:05 +0700

    'commit'

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,18 @@
-let getHomepage = (req, res) => {
-    return res.render('homepag.ejs');
+
+import db from '../models/index';
+let getHomepage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+
+
+
+    } catch (e) {
+        console.log(e)
+    }
+
 }
 let getAboutPage = (req, res) => {
     return res.render('test/aboutme.ejs');
